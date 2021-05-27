e99d26f27a304c6e34345b11405189879f0b4f96
Daiane Silva <daianemsilva09@gmail.com>
2021-05-27 13:14:32 -0300

    Aula 01 - 06

---
+++ course-manager/src/app/courses/course-list.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { Course } from './course';
+
+@Component({
+    selector: 'app-course-list',
+    templateUrl: './course-list.component.html'
+
+})
+
+
+export class CourseListComponent implements OnInit {
+    courses: Course[] = [];
+
+    ngOnInit(): void {
+        this.courses = [
+            {
+                id: 1,
+                name: 'Angular',
+                imageUrl: '',
+                price: 99.99,
+                code: 'xps-8796',
+                duration: 120,
+                rating: 5.4,
+                releaseDate: 'december, 2, 2019'
+
+
+            },
+            {
+                id: 2,
+                name: 'Angular: HTTP',
+                imageUrl: '',
+                price: 90.00,
+                code: 'lkl-1094',
+                duration: 80,
+                rating: 4,
+                releaseDate: 'November, 2, 2019'
+
+            }
+        ]
+    }
+
+}
