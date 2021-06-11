0ba296db9349f9d941255a491a0e9d8d01958423
Daiane Silva <daianemsilva09@gmail.com>
2021-06-11 16:47:25 -0300

    teste

--- course-manager/src/app/courses/course-list.component.ts
+++ course-manager/src/app/courses/course-list.component.ts
@@ -16,7 +16,7 @@ export class CourseListComponent implements OnInit {
             {
                 id: 1,
                 name: 'Angular',
-                imageUrl: '',
+                imageUrl: '/assets/images/forms.png',
                 price: 99.99,
                 code: 'xps-8796',
                 duration: 120,
@@ -28,7 +28,7 @@ export class CourseListComponent implements OnInit {
             {
                 id: 2,
                 name: 'Angular: HTTP',
-                imageUrl: '',
+                imageUrl: '/assets/images/http.png',
                 price: 90.00,
                 code: 'lkl-1094',
                 duration: 80,
